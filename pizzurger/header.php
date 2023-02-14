dd096397cb81387da4b6e9d8c2a2353d29bdf3ed
dadaJunglist <kavalliero@gmail.com>
2023-02-14 11:27:50 +0200

    folder

---
+++ pizzurger/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ranga&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inria+Serif:wght@300&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="grid-container">
+
+    <div class="header">
+        <div style="flex-grow: 3; vertical-align: middle; "><a href="contact.html" class="button vcenter">Contact</a></div>
+        <div style="flex-grow: 3; vertical-align: middle; "><a href="staff.html" class="button vcenter">Staff</a> </div>
+        <div style="flex-grow: 3; vertical-align: middle; "><a href="reserve.html" class="button vcenter">Reserve</a> </div>
+        <div style="flex-grow: 3; vertical-align: middle; "><a href="menu.html" class="button vcenter">Menu</a> </div>
+        <div style="flex-grow: 5;"></div>
+        <div class="visible" style="flex-grow: 1;"><p>Pizz</p><p1>urgeR</p1></div> 
+        <div style="flex-grow: 2;"> <a href="index.html"><img style="width: 40px; padding: 5px;" class="vcenter" src="images/logo.png" alt="logo"></a></div> 
+    </div>
---
+++ pizzurger/index.php
@@ -0,0 +1,30 @@
+<?php include 'header.php'; ?>
+    
+    <div class="main">
+        
+        <div class="cntrblock">
+            <h1>The only place to eat in Hämeenlinna! </h1>
+        </div>
+        
+        <div class="imgblock"><img src="images/burger1.jpg" alt="burger"></div>
+        <div class="txtblock">Over the fire cooked burgers with perfectly balanced combinations of fresh ingredients</div>
+            
+        <div class="invisible">~~~~~~~~</div>
+        <div class="txtblock">Pizzas cooked in medieval stove, always hot for our guests</div>
+        <div class="imgblock"><img src="images/pizza1.jpg" alt="pizza"></div>
+       
+
+        <div class="imgblock"><img src="images/pizza2.jpg" alt="pizza"></div>
+        <div class="txtblock">We know best combinations of flavors. There is no secret - it is an art of our chef.</div>
+        <div class="invisible">~~~~~~~~</div>
+        <div class="txtblock">Can you get anything better elsewhere?</div>
+        <div class="imgblock"><img src="./images/pizza3.jpg" alt="pizza"></div>
+
+           
+        <div class="cntrblock">
+            <iframe class="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1934.0928626833834!2d24.4601133!3d61.0034388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x468e5dbef602a7e1%3A0xcacad440ad4f93de!2sKustaa%20III%3An%20katu%206%2C%2013100%20H%C3%A4meenlinna!5e0!3m2!1sfi!2sfi!4v1674664626136!5m2!1sfi!2sfi" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        
+    </div>
+    
+<?php include 'footer.php'; ?>
