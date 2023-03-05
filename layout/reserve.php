c93b62b568214a7aadafbacdf0723a8eb299f35a
Welkro <royliu29@gmail.com>
2023-03-05 17:10:58 +0200

    Updated reserve.php

--- layout/reserve.php
+++ layout/reserve.php
@@ -1,12 +1,13 @@
 <?php include "header.php"?>
         
         <div class="reservationform">
-            <form action="">
-                <h1>Reservation Form</h1>     
+            <form method="post" action="">
+                <h1>Reservation Form</h1>  
+                <!--   
                 <p>Name</p>
                 <input type="text" name="fname" placeholder="First name" required >
                 <input type="text" name="lname" placeholder="Last name" required >
-        
+                -->
                 <p>Reservation date & time</p>
                     <input type="datetime-local" name="time" required>
      
@@ -19,14 +20,16 @@
 
     <?php
  if (isset($_POST['submit'])){
+    /*
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
+    */
     $time = $_POST['time'];
     $text = $_POST['text'];
 
-    include "db.php";
-    $sql = "insert into reservation (fname, lname, datetime, text)
-    values('$fname', '$lname', '$time', '$text')";
+    include 'db.php';
+    $sql = "insert into reservation (time, text)
+    values( '$time', '$text')";
 
     if ($connection ->query($sql) === TRUE){
         echo "Your information is added successfully";
